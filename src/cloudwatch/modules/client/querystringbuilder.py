4831271fffdc8869d49aedf043f05fc985ad4b8d
Matthew Glubb <matthew.glubb@madebykite.com>
2020-06-04 09:01:16 +0100

    Round floating point precision to 8 decimal places

--- src/cloudwatch/modules/client/querystringbuilder.py
+++ src/cloudwatch/modules/client/querystringbuilder.py
@@ -6,8 +6,8 @@ from urllib import urlencode
 
 class QuerystringBuilder(object):
     """
-    The querystring builder is responsible for creating a querystring from MetricData objects 
-    and additional request parameters.  
+    The querystring builder is responsible for creating a querystring from MetricData objects
+    and additional request parameters.
     """
     _LOGGER = get_logger(__name__)
     _METRIC_PREFIX = "MetricData.member."
@@ -25,10 +25,10 @@ class QuerystringBuilder(object):
 
     def __init__(self, enable_high_resolution_metrics=False):
         self.enable_high_resolution_metrics = enable_high_resolution_metrics
-   
+
     def build_querystring(self, metric_list, request_map):
         """
-        Creates querystring from list of MetricData objects and a map of request key value pairs 
+        Creates querystring from list of MetricData objects and a map of request key value pairs
         with all keys sorted in ascending order as required by CloudWatch.
         """
         metric_map = self._build_metric_map(metric_list)
@@ -41,15 +41,15 @@ class QuerystringBuilder(object):
         base_map.update(call_map)
         sorted_query_data = sorted(base_map.items(),key=operator.itemgetter(0))
         # by default urlencode replace spaces with '+' but CloudWatch requires them to be encoded to '%20'
-        url_string = urlencode(sorted_query_data).replace('+', '%20') 
+        url_string = urlencode(sorted_query_data).replace('+', '%20')
         return url_string
 
 
     def _build_metric_map(self, metric_list):
-        """ 
-        Translate the list of metric objects into a single map with keys represented in the format required 
+        """
+        Translate the list of metric objects into a single map with keys represented in the format required
         by CloudWatch querystring.
-        """   
+        """
         metric_map = {}
         metric_index = 1
         for metric in metric_list:
@@ -62,7 +62,7 @@ class QuerystringBuilder(object):
             self._add_values(metric, metric_map, metric_prefix)
             metric_index += 1
         return metric_map
-    
+
     def _add_dimensions(self, metric, metric_map, metric_prefix):
         dimension_index = 1
         for dimension_key in metric.dimensions.keys():
@@ -70,13 +70,13 @@ class QuerystringBuilder(object):
             metric_map[dimension_prefix + self._NAME_KEY] = dimension_key
             metric_map[dimension_prefix + self._VALUE_KEY] = metric.dimensions[dimension_key]
             dimension_index += 1
-    
+
     def _add_values(self, metric, metric_map, metric_prefix):
         if not metric.statistics:
             msg = "Missing value for metric " + metric.metric_name
             self._LOGGER.warning(msg)
             raise ValueError(msg)
-        metric_map[metric_prefix + self._STAT_MAX] = metric.statistics.max
-        metric_map[metric_prefix + self._STAT_MIN] = metric.statistics.min
-        metric_map[metric_prefix + self._STAT_SUM] = metric.statistics.sum
+        metric_map[metric_prefix + self._STAT_MAX] = round(metric.statistics.max, 8)
+        metric_map[metric_prefix + self._STAT_MIN] = round(metric.statistics.min, 8)
+        metric_map[metric_prefix + self._STAT_SUM] = round(metric.statistics.sum, 8)
         metric_map[metric_prefix + self._STAT_SAMPLE] = metric.statistics.sample_count
